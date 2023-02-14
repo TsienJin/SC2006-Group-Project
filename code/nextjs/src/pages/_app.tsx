f7f526887b64cfa5f8d68f73fa950c421af080f2
Tsien Jin <tsien.jin@gmail.com>
2023-02-14 23:06:13 +0800

    Updated to persist state after refresh

--- code/nextjs/src/pages/_app.tsx
+++ code/nextjs/src/pages/_app.tsx
@@ -2,13 +2,16 @@ import '@/styles/globals.css'
 import 'mapbox-gl/dist/mapbox-gl.css'
 import type { AppProps } from 'next/app'
 
-import { store } from '@/store'
+import { store, persistor } from '@/store'
 import { Provider } from 'react-redux'
+import { PersistGate } from 'redux-persist/integration/react'
 
 export default function App({ Component, pageProps }: AppProps) {
   return(
     <Provider store={store}>
-      <Component {...pageProps} />
+      <PersistGate persistor={persistor} loading={null}>
+        <Component {...pageProps} />
+      </PersistGate>
     </Provider>
   )
 }
--- code/nextjs/src/store.ts
+++ code/nextjs/src/store.ts
@@ -1,23 +1,40 @@
-import { configureStore } from '@reduxjs/toolkit'
+import {combineReducers, configureStore} from '@reduxjs/toolkit'
 import counterReducer from '@/components/slice/test'
 import menuReducer from '@/components/slice/menu'
 import sideBarReducer from '@/components/slice/sideBar'
 import userReducer from '@/components/slice/user'
 import systemReducer from '@/components/slice/system'
 import routeReducer from '@/components/slice/route'
-import * as process from "process";
+
+import * as process from "process"
+import thunk from 'redux-thunk'
+import logger from 'redux-logger'
+import { persistStore, persistReducer } from 'redux-persist'
+import storage from 'redux-persist/lib/storage'
+
+const allReducers = combineReducers({
+  counter: counterReducer,
+  menu: menuReducer,
+  sideBar: sideBarReducer,
+  user: userReducer,
+  system: systemReducer,
+  route: routeReducer
+})
+
+const persistConfig = {
+  key: 'root',
+  storage,
+}
+
+const persistedReducer = persistReducer(persistConfig, allReducers)
 
 export const store = configureStore({
-  reducer: {
-    counter: counterReducer,
-    menu: menuReducer,
-    sideBar: sideBarReducer,
-    user: userReducer,
-    system: systemReducer,
-    route: routeReducer
-  },
-  devTools: process.env.NODE_ENV != "production",
+  reducer: persistedReducer,
+  devTools: process.env.NODE_ENV !== 'production',
+  middleware: [thunk]
 })
+
+export const persistor = persistStore(store)
 
 // Infer the `RootState` and `AppDispatch` types from the store itself
 export type RootState = ReturnType<typeof store.getState>
