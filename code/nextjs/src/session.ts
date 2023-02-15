5f54641d5392269de416a0e8fbe955584eed638c
Tsien Jin <tsien.jin@gmail.com>
2023-02-15 11:26:54 +0800

    Added sessions object

---
+++ code/nextjs/src/session.ts
@@ -0,0 +1,49 @@
+import type { SessionStore } from "next-session"
+import {SessionData} from "next-session/lib/types";
+
+
+export enum sessionKeys {
+  Test="test",
+}
+
+
+
+
+export class MemoryStore implements SessionStore {
+  store: Map<string, string>;
+
+  constructor() {
+    this.store = new Map();
+  }
+
+  async get(sid: string|sessionKeys): Promise<SessionData | null> {
+    const sess = this.store.get(sid);
+    if (sess) {
+      const session = JSON.parse(sess, (key, value) => {
+        if (key === "expires") return new Date(value);
+        return value;
+      }) as SessionData;
+      if (
+        session.cookie.expires &&
+        session.cookie.expires.getTime() <= Date.now()
+      ) {
+        await this.destroy(sid);
+        return null;
+      }
+      return session;
+    }
+    return null;
+  }
+
+  async set(sid: string|sessionKeys, sess: SessionData) {
+    this.store.set(sid, JSON.stringify(sess));
+  }
+
+  async destroy(sid: string) {
+    this.store.delete(sid);
+  }
+
+  async touch(sid: string, sess: SessionData) {
+    this.store.set(sid, JSON.stringify(sess));
+  }
+}
