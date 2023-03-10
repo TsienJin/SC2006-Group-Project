85380497354e7dc1f9fd83dbcd45f4e09c994802
Tsien Jin <tsien.jin@gmail.com>
2023-03-10 23:53:03 +0800

    Added noti

--- code/nextjs/src/pages/index.tsx
+++ code/nextjs/src/pages/index.tsx
@@ -2,6 +2,7 @@ import HeadComponent from '@/components/seo/HeadComponent'
 import Map from '@/components/mapbox/Map'
 import Menu from '@/components/overlays/menu/Menu'
 import SideBar from '@/components/overlays/sideBar/SideBar'
+import Notification from "@/components/overlays/notification/Notification";
 
 export default function Home() {
   return (
@@ -9,6 +10,7 @@ export default function Home() {
       <HeadComponent title={'NaviLoo | Find Toilet Locations on the Go'} description={'Find your way from point A to B with ease using our navigation tool. Plus, never worry about finding a toilet with our toilet locator feature.'} />
       <div className='w-[100dvw] h-[100dvh] relative'>
         <SideBar />
+        <Notification />
         <Menu />
         <Map />
       </div>
