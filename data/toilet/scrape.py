925f017eee7fa8fb2ba9ae112ecfb3abac4bde02
Tsien Jin <tsien.jin@gmail.com>
2023-03-21 21:00:05 +0800

    Changes

--- data/toilet/scrape.py
+++ data/toilet/scrape.py
@@ -6,80 +6,80 @@ from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.common.by import By
 
 
-
 class Writer:
-  def __init__(self) -> None:
-    pass
+    def __init__(self, output: str) -> None:
+        self.output = output
+        pass
 
+    def write(self, out: str):
+        with open(self.output, "a") as f:
+            f.write(out + "\n")
 
 
 class Toilet:
-  def __init__(self, element) -> None:
-    self.element = element
-
-    self.type = self.element[0].text
-    self.name = self.element[1].text
-    self.addr = re.sub(',', '', self.element[2].text)
-    self.link = ""
+    def __init__(self, element) -> None:
+        self.element = element
 
 
-    try:
-      self.link = self.element[1].find_element(By.TAG_NAME, 'a').get_attribute('href')
-    except Exception as e:
-      self.link = ""
-          
-    
-  def toCSV(self) -> str:
-    return f"{self.name},{self.addr},{self.type},{self.link}"
-    
+        self.type = self.element[0].text
+        self.name = self.element[1].text
+        self.addr = re.sub(',', '', self.element[2].text)
+        self.link = ""
 
+        try:
+            self.link = self.element[1].find_element(By.TAG_NAME, 'a').get_attribute('href')
+        except Exception as e:
+            self.link = ""
+
+    def toCSV(self) -> str:
+        return f"{self.name},{self.addr},{self.type},{self.link}"
 
 
 class App:
-  def __init__(self) -> None:
-    self.target = "https://www.toilet.org.sg/loomapdirectory"
-    self.driver = webdriver.Firefox()
-    
-    
-  def __connect(self, url) -> None:
-    try:
-      self.driver.get(url)
-    except Exception as e:
-      print(e)
-      
-  def __showTables(self) -> None:
-    self.driver.execute_script("""
+    def __init__(self) -> None:
+        self.target = "https://www.toilet.org.sg/loomapdirectory"
+        self.driver = webdriver.Firefox()
+        self.writer = Writer("output.csv")
+
+
+    def __connect(self, url) -> None:
+        try:
+            self.driver.get(url)
+        except Exception as e:
+            print(e)
+
+    def __showTables(self) -> None:
+        self.driver.execute_script("""
                               $('ul.tabs-content').children().addClass('active')
                                """)
-      
-  def forceClose(self) -> None:
-    self.driver.quit()
-      
-  def main(self) -> None:
-    self.__connect(self.target)
-    self.__showTables()
-    
-    elements = self.driver.find_elements(By.CLASS_NAME, "tab__content")
 
-    for element in elements:
-      tableBody = element.find_element(By.TAG_NAME, 'tbody')
-      rows = tableBody.find_elements(By.TAG_NAME, 'tr')
-      
-      for row in rows:
-        cells = row.find_elements(By.TAG_NAME, 'td')
-        thing = Toilet(cells)
-        print(thing.toCSV())
-      
-    
+    def forceClose(self) -> None:
+        self.driver.quit()
 
-    
-    
-    
+    def main(self) -> None:
+        self.__connect(self.target)
+        self.__showTables()
+
+        elements = self.driver.find_elements(By.CLASS_NAME, "tab__content")
+
+        for element in elements:
+            tableBody = element.find_element(By.TAG_NAME, 'tbody')
+            rows = tableBody.find_elements(By.TAG_NAME, 'tr')
+
+            for row in rows:
+                cells = row.find_elements(By.TAG_NAME, 'td')
+                thing = Toilet(cells)
+                self.writer.write(thing.toCSV())
+                print(thing.toCSV())
+
+
 if __name__ == "__main__":
-  app = App()
-  
-  try:
-    app.main()
-  finally:
-    app.forceClose()
-    print('done')
+    app = App()
+
+    try:
+        app.main()
+    except:
+        print("oops")
+    finally:
+        app.forceClose()
+        print('done')
