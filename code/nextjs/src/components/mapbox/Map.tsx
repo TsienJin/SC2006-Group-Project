28f2ccd2a785153d76b5a7221413db51624dd41f
Tsien Jin <tsien.jin@gmail.com>
2023-03-21 20:41:15 +0800

    Converted from mapboxgl to react-mapgl
    testing added popups and other stuff too

--- code/nextjs/src/components/mapbox/Map.tsx
+++ code/nextjs/src/components/mapbox/Map.tsx
@@ -1,52 +1,65 @@
-import mapboxgl from 'mapbox-gl'
-import { useEffect, useRef } from 'react'
+import Mapbox, {GeolocateControl, Marker, Popup} from 'react-map-gl'
+import {useState} from "react";
+
+
+
+
+
+const Test = () => {
+
+  const [open, setOpen] = useState<boolean>(false)
+
+  const toggle = () => {
+    setOpen(!open)
+  }
+
+  return(
+    <>
+      <Marker longitude={103.6831} latitude={1.3483} onClick={toggle}>
+        <div className={`transition-all rounded-full border-2 border-white w-7 h-7 flex flex-row justify-center items-center ${open?"bg-rust":"bg-violet"}`}>
+          <span className={"font-bold text-lg text-white"}>T</span>
+        </div>
+      </Marker>
+      {open &&
+          <Popup
+              longitude={103.6831}
+              latitude={1.3483}
+              anchor={"bottom"}
+              closeOnClick={false}
+              closeOnMove={false}
+              closeButton={false}
+              offset={12}
+          >
+              <div>
+                  pooop
+              </div>
+          </Popup>
+      }
+    </>
+  )
+}
+
+
+
 
 const Map = () => {
 
-  // setting up const's
-  const mapContainer = useRef<any>(null)
-  const map  = useRef<mapboxgl.Map | any>(null)
-
-  // on load
-  useEffect(() => {
-    // set access token
-    mapboxgl.accessToken = process.env.NEXT_PUBLIC_MAPBOX_KEY ?? '' 
-
-    // init map
-    map.current = new mapboxgl.Map({
-      container: mapContainer.current,
-      style: process.env.NEXT_PUBLIC_MAPBOX_STYLE,
-      center: [103.808052586332, 1.3516161224392],
-      zoom: 11
-    })
-
-    map.current.addControl(
-      new mapboxgl.GeolocateControl({
-        positionOptions: {
-          enableHighAccuracy: true
-        },
-        // When active the map will receive updates to the device's location as it changes.
-        trackUserLocation: true,
-        // Draw an arrow next to the location dot to indicate which direction the device is heading.
-        showUserHeading: true
-      })
-    )
-
-
-
-
-
-    // destroy element
-    return () => {
-    
-    }
-  }, [])
-  
-
-
-
   return(
-    <div className='mapElement w-full h-full' ref={mapContainer} />
+    <>
+      <Mapbox
+        initialViewState={{
+          longitude: 103.808052586332,
+          latitude: 1.3516161224392,
+          zoom: 11
+      }}
+
+        mapStyle={process.env.NEXT_PUBLIC_MAPBOX_STYLE}
+        mapboxAccessToken={process.env.NEXT_PUBLIC_MAPBOX_KEY}
+      >
+        <GeolocateControl showAccuracyCircle={true} showUserHeading={true} />
+        <Test />
+      </Mapbox>
+    </>
   )
 }
 
