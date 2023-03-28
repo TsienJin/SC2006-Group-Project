06fdd912d634c42c29d496752ca8b4bb18c6463d
Tsien Jin <tsien.jin@gmail.com>
2023-03-28 23:13:20 +0800

    Refactored

--- code/nextjs/src/components/mapbox/Map.tsx
+++ code/nextjs/src/components/mapbox/Map.tsx
@@ -3,44 +3,12 @@ import {useEffect, useRef, useState} from "react";
 import {useSelector} from "react-redux";
 import {RootState} from "@/store";
 import mapboxgl from "mapbox-gl";
+import Test from "@/components/mapbox/Markers/test";
+import RouteLocationMarker from "@/components/mapbox/Markers/routeLocation";
 
 
 
 
-
-const Test = () => {
-
-  const [open, setOpen] = useState<boolean>(false)
-
-  const toggle = () => {
-    setOpen(!open)
-  }
-
-  return(
-    <>
-      <Marker longitude={103.6831} latitude={1.3483} onClick={toggle}>
-        <div className={`transition-all rounded-full border-2 border-white w-7 h-7 flex flex-row justify-center items-center ${open?"bg-rust":"bg-violet"}`}>
-          <span className={"font-bold text-lg text-white"}>T</span>
-        </div>
-      </Marker>
-      {open &&
-          <Popup
-              longitude={103.6831}
-              latitude={1.3483}
-              anchor={"bottom"}
-              closeOnClick={false}
-              closeOnMove={false}
-              closeButton={false}
-              offset={12}
-          >
-              <div>
-                  pooop
-              </div>
-          </Popup>
-      }
-    </>
-  )
-}
 
 
 
@@ -49,6 +17,7 @@ const Map = () => {
 
 
   const zoomLoc = useSelector((state:RootState) => state.location.coords)
+  const routeLoc = useSelector((state:RootState) => state.route)
 
   const mapRef = useRef(null)
 
@@ -84,6 +53,7 @@ const Map = () => {
       >
         <GeolocateControl showAccuracyCircle={true} showUserHeading={true} />
         <Test />
+        <RouteLocationMarker />
       </Mapbox>
     </>
   )
