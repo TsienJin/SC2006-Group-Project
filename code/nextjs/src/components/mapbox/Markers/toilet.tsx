62ba7922c6597839bb79e8f156185a1ecd15a45a
Tsien Jin <tsien.jin@gmail.com>
2023-03-28 23:13:03 +0800

    Refactoring stuff and all

---
+++ code/nextjs/src/components/mapbox/Markers/toilet.tsx
@@ -0,0 +1,38 @@
+import {useState} from "react";
+import {Marker, Popup} from "react-map-gl";
+
+const ToiletMarker = () => {
+
+  const [open, setOpen] = useState<boolean>(false)
+
+  const toggle = () => {
+    setOpen(!open)
+  }
+
+  return(
+    <>
+      <Marker longitude={103.6831} latitude={1.3483} onClick={toggle}>
+        <div className={`transition-all rounded-full border-2 border-white w-7 h-7 flex flex-row justify-center items-center ${open?"bg-rust":"bg-violet"}`}>
+          <span className={"font-bold text-lg text-white"}>T</span>
+        </div>
+      </Marker>
+      {open &&
+          <Popup
+              longitude={103.6831}
+              latitude={1.3483}
+              anchor={"bottom"}
+              closeOnClick={false}
+              closeOnMove={false}
+              closeButton={false}
+              offset={12}
+          >
+              <div>
+                  pooop
+              </div>
+          </Popup>
+      }
+    </>
+  )
+}
+
+export default ToiletMarker
