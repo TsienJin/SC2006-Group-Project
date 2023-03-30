641c0dbbc2956f5359ba76dac1ef50ba61e54246
Tsien Jin <tsien.jin@gmail.com>
2023-03-30 22:37:11 +0800

    Added cursors pointer

--- code/nextjs/src/components/mapbox/Markers/Traffic.tsx
+++ code/nextjs/src/components/mapbox/Markers/Traffic.tsx
@@ -30,7 +30,7 @@ const Mark = ({incident}:{incident:TrafficIncident}) => {
   return(
     <>
       <Marker longitude={incident.coordinates.longitude} latitude={incident.coordinates.latitude} onClick={toggle}>
-        <div className={"bg-red-600 rounded-full overflow-hidden text-offwhite"}>
+        <div className={"bg-red-600 rounded-full overflow-hidden text-offwhite cursor-pointer"}>
           <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6">
             <path strokeLinecap="round" strokeLinejoin="round" d="M12 9v3.75m9-.75a9 9 0 11-18 0 9 9 0 0118 0zm-9 3.75h.008v.008H12v-.008z" />
           </svg>
