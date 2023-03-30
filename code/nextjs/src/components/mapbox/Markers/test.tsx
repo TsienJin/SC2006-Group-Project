dca96ca956bb9d2cdd338ac6c1638fb1d47f24bd
Tsien Jin <tsien.jin@gmail.com>
2023-03-30 22:36:59 +0800

    Updated cursor

--- code/nextjs/src/components/mapbox/Markers/test.tsx
+++ code/nextjs/src/components/mapbox/Markers/test.tsx
@@ -12,7 +12,7 @@ const Test = () => {
   return(
     <>
       <Marker longitude={103.6831} latitude={1.3483} onClick={toggle}>
-        <div className={`transition-all rounded-full border-2 border-white w-7 h-7 flex flex-row justify-center items-center ${open?"bg-rust":"bg-violet"}`}>
+        <div className={`transition-all rounded-full cursor-pointer border-2 border-white w-7 h-7 flex flex-row justify-center items-center ${open?"bg-rust":"bg-violet"}`}>
           <span className={"font-bold text-lg text-white"}>T</span>
         </div>
       </Marker>
