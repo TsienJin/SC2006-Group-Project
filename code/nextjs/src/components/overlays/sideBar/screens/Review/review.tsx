47625f00935b5cfc3ed402ff1f9997ddf7b5df04
Tsien Jin <tsien.jin@gmail.com>
2023-04-01 15:58:48 +0800

    Added function to send review, needs debugging

--- code/nextjs/src/components/overlays/sideBar/screens/Review/review.tsx
+++ code/nextjs/src/components/overlays/sideBar/screens/Review/review.tsx
@@ -1,7 +1,7 @@
 import FormWrapper from "@/components/fields/FormWrapper";
 import TextInput from "@/components/fields/TextInput";
-import Button, {buttonColourBlue} from "@/components/clickeable/Button";
-import SelectInput, {ratingOptions} from "@/components/fields/SelectInput";
+import Button, {buttonColourBlue, buttonColourGreen} from "@/components/clickeable/Button";
+import SelectInput, {emptyOption, Option, ratingOptions} from "@/components/fields/SelectInput";
 import FieldWrapper from "@/components/fields/FieldWrapper";
 import {useDispatch, useSelector} from "react-redux";
 import {RootState} from "@/store";
@@ -9,16 +9,57 @@ import {addNoti, createNoti, notiType} from "@/components/slice/notification";
 import AccountLoginScreen from "@/components/overlays/sideBar/screens/Account/AccountLogin";
 import {useEffect, useState} from "react";
 import {addToStack, popLatest, sideBarStatesEnum} from "@/components/slice/sideBar";
+import {Action} from "@/components/clickeable/types";
+import {Hoist} from "@/components/fields/types";
+import {postMiddleware} from "@/middleware/middleware";
+import {middlewareOptions} from "@/middleware/types";
 
 
 const ReviewScreen = () => {
 
   const toilet = useSelector((state:RootState) => state.toiletInterest)
   const userState = useSelector((state:RootState) => state.user)
-
   const [userLatch, setUserLatch] = useState<typeof userState>(userState)
-
   const dispatch = useDispatch()
+
+  const [rating, setRating] = useState<Option>(emptyOption)
+  const [review, setReview] = useState<string>("")
+
+
+  const ratingHoist:Hoist<Option> = (value) => {
+    setRating(value)
+  }
+
+  const reviewHoist:Hoist<string> = (value) => {
+    setReview(value)
+  }
+
+
+  const submitReview:Action = () => {
+
+    const payload:any = {
+      longitude: toilet.Address.coordinates.longitude,
+      latitude: toilet.Address.coordinates.latitude,
+      rating: rating.value,
+      comment: review
+    }
+
+    console.log(payload)
+
+    const options:middlewareOptions = {
+      endpoint: `${process.env.NEXT_PUBLIC_BACKEND}/reviews/create/`,
+      params: payload
+    }
+
+    postMiddleware(options)
+      .then(r=>{
+        console.log(r)
+      })
+      .catch(e=>{
+        console.error(e)
+      })
+
+  }
 
 
 
@@ -45,13 +86,13 @@ const ReviewScreen = () => {
   }
 
   return(
-    <FormWrapper>
+    <FormWrapper action={submitReview}>
       <FieldWrapper>
         <span className={"font-medium text-xl"}>{toilet.Address.name || toilet.Address.address}</span>
       </FieldWrapper>
-      <SelectInput placeholder={"Rating"} options={ratingOptions} />
-      <TextInput placeholder={"Review"} type={"textarea"} />
-      <Button text={"Submit"} colour={buttonColourBlue} />
+      <SelectInput placeholder={"Rating"} options={ratingOptions} hoist={ratingHoist}/>
+      <TextInput placeholder={"Review"} type={"textarea"} hoist={reviewHoist}/>
+      <Button text={"Submit"} colour={buttonColourGreen} />
     </FormWrapper>
   )
 }
