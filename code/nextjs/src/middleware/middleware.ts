367b9d41b991aa423ec05c58376b917b0293056a
Tsien Jin <tsien.jin@gmail.com>
2023-04-02 15:38:04 +0800

    allows credentials

--- code/nextjs/src/middleware/middleware.ts
+++ code/nextjs/src/middleware/middleware.ts
@@ -17,7 +17,7 @@ export async function postMiddleware(options:middlewareOptions, sendCredential:b
             // 'Access-Control-Allow-Methods': 'GET, POST, PATCH, PUT, DELETE, OPTIONS',
             ...options.headers,
           },
-        // withCredentials: sendCredential,
+        withCredentials: sendCredential,
       }
     )
 
