a283bb19b03a5722711ccaaf926b0bad8e128a4d
Tsien Jin <tsien.jin@gmail.com>
2023-04-04 01:37:27 +0800

    Update toilet.tsx

--- code/nextjs/src/components/mapbox/Markers/toilet.tsx
+++ code/nextjs/src/components/mapbox/Markers/toilet.tsx
@@ -8,9 +8,12 @@ import {Action} from "@/components/clickeable/types";
 import {addToStack, popLatest, popStack, setState, sideBarStatesEnum} from "@/components/slice/sideBar";
 import {RootState} from "@/store";
 import {setToiletInterest} from "@/components/slice/toiletInterest";
+import {middlewareOptions} from "@/middleware/types";
+import {postMiddleware} from "@/middleware/middleware";
 
 
 export type ToiletInfo = {
+  averageRating: number;
   Address: {
     name: string,
     address: string,
@@ -27,7 +30,7 @@ export type ToiletInfo = {
       description: string,
     },
     reviews: [],
-    averageRating: number
+    // averageRating: number
   }
 }
 
@@ -63,9 +66,12 @@ const ToiletPopup = ({toilet}:{toilet:ToiletInfo}) => {
 
   const dispatch = useDispatch()
   const sidebarState = useSelector((state:RootState) => state.sideBar)
+  const userState = useSelector((state:RootState) => state.user)
 
 
   const reviewToilet:Action = () => {
+
+    console.log(toilet)
 
     if(sidebarState.state == sideBarStatesEnum.None){
       dispatch(popStack())
@@ -75,6 +81,45 @@ const ToiletPopup = ({toilet}:{toilet:ToiletInfo}) => {
     dispatch(setState(sideBarStatesEnum.Review))
     dispatch(popLatest(sideBarStatesEnum.Review))
     dispatch(addToStack(sideBarStatesEnum.Review))
+  }
+
+
+
+  const addFavToilet:Action = () => {
+    if(userState.id==""){
+      dispatch(addNoti(createNoti(
+        "Not logged in!",
+        "In order to add to favourites, you have to be logged in!",
+        notiType.Warning
+      )))
+    } else {
+      const options:middlewareOptions = {
+        endpoint: `${process.env.NEXT_PUBLIC_BACKEND}/toilets/addfavourite/`,
+        params: {
+          userID: userState.id,
+          latitude: toilet.Address.coordinates.latitude,
+          longitude: toilet.Address.coordinates.longitude
+        },
+      }
+
+      postMiddleware(options)
+        .then(r=>{
+          console.log(r)
+          dispatch(addNoti(createNoti(
+            "Added to favourites!",
+            "Access your favourite toilets using the favourites menu item!",
+            notiType.Notification
+          )))
+        })
+        .catch(e=>{
+          console.error(e)
+          dispatch(addNoti(createNoti(
+            "Oops! Error adding to favourite!",
+            "Lets pretend this didn't happen.",
+            notiType.Warning
+          )))
+        })
+    }
   }
 
 
@@ -106,9 +151,10 @@ const ToiletPopup = ({toilet}:{toilet:ToiletInfo}) => {
     >
       <div className={"flex flex-col justify-center items-center max-w-[45ch]"}>
         <div className={"top flex flex-col justify-start items-start"}>
-          <div className={"flex flex-row justify-start items-center"}>
+          <div className={"flex flex-row justify-start items-start gap-x-2"}>
             <span className={"text-shadow font-medium text-md"}>{toilet.Address.name.length>0? toilet.Address.name : "Toilet"}</span>
-            <span>{toilet.Address.averageRating?`${toilet.Address.averageRating} ⭐`:""}</span>
+            <span className={"flex-shrink-0 text-xl"}>{toilet.averageRating>0?`${'⭐'.repeat(parseInt(`${toilet.averageRating}`))} `:""}</span>
+            {/*<span>{toilet.averageRating}</span>*/}
           </div>
           <div className={"flex flex-col"}>
             <span>{toilet.Address.address}</span>
@@ -124,7 +170,7 @@ const ToiletPopup = ({toilet}:{toilet:ToiletInfo}) => {
               <path strokeLinecap="round" strokeLinejoin="round" d="M2.25 12.76c0 1.6 1.123 2.994 2.707 3.227 1.068.157 2.148.279 3.238.364.466.037.893.281 1.153.671L12 21l2.652-3.978c.26-.39.687-.634 1.153-.67 1.09-.086 2.17-.208 3.238-.365 1.584-.233 2.707-1.626 2.707-3.228V6.741c0-1.602-1.123-2.995-2.707-3.228A48.394 48.394 0 0012 3c-2.392 0-4.744.175-7.043.513C3.373 3.746 2.25 5.14 2.25 6.741v6.018z" />
             </svg>
           </PopupButton>
-          <PopupButton>
+          <PopupButton action={addFavToilet}>
             <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6">
               <path strokeLinecap="round" strokeLinejoin="round" d="M17.593 3.322c1.1.128 1.907 1.077 1.907 2.185V21L12 17.25 4.5 21V5.507c0-1.108.806-2.057 1.907-2.185a48.507 48.507 0 0111.186 0z" />
             </svg>
