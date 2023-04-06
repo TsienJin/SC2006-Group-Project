751c46234b9410197bed9673a930c84cc8abd822
Tsien Jin <tsien.jin@gmail.com>
2023-04-06 18:27:06 +0800

    Added button to force update toilets on the backend

--- code/nextjs/src/components/overlays/sideBar/screens/Settings/Settings.tsx
+++ code/nextjs/src/components/overlays/sideBar/screens/Settings/Settings.tsx
@@ -3,17 +3,24 @@ import SelectInput, {Option, SelectInputOptions} from "@/components/fields/Selec
 import Checkbox from "@/components/clickeable/Checkbox";
 import {Hoist} from "@/components/fields/types";
 import {useDispatch, useSelector} from "react-redux";
-import {setLang, setToilet, setTraffic} from "@/components/slice/system";
+import {setToilet, setTraffic} from "@/components/slice/system";
 import {RootState} from "@/store";
-import Tab from "@/components/clickeable/Tab";
 import {Action, link} from "@/components/clickeable/types";
 import TabLink from "@/components/clickeable/TabLink";
+import Button, {buttonColourRust} from "@/components/clickeable/Button";
+import {useState} from "react";
+import {middlewareOptions} from "@/middleware/types";
+import {getMiddleWare} from "@/middleware/middleware";
+import {addNoti, createNoti, notiType} from "@/components/slice/notification";
 
 
 const SettingScreen = () => {
 
   const systemState = useSelector((state:RootState) => state.system)
   const dispatch = useDispatch()
+
+  const [updateToiletButtonText, setUpdateToiletButtonText] = useState<string>("EXPENSIVE! Update Toilets")
+  const [allowUpdateToilet, setAllowUpdateToilet] = useState<boolean>(true)
 
   const langOptions:SelectInputOptions = {
     options: [],
@@ -41,6 +48,45 @@ const SettingScreen = () => {
     text: 'Report them here'
   }
 
+  const updateToilet:Action = () => {
+    if(allowUpdateToilet){
+      const options:middlewareOptions = {
+        endpoint: `${process.env.NEXT_PUBLIC_BACKEND}/settings/updatetoilet/`
+      }
+
+      dispatch(addNoti(createNoti(
+        "Fetching updated toilets!",
+        "This might take a while...",
+        notiType.Notification
+      )))
+
+      setAllowUpdateToilet(false)
+      setUpdateToiletButtonText("Loading...")
+
+      getMiddleWare(options)
+        .then(r=>{
+          console.log(r)
+          dispatch(addNoti(createNoti(
+            "Updated toilets!",
+            "Refresh to view updates! There goes ~1000 API calls :(",
+            notiType.Notification
+          )))
+        })
+        .catch(e=>{
+          console.error(e)
+          dispatch(addNoti(createNoti(
+            "Error updating toilets!",
+            "Something went wrong fetching the toilet data from the backend",
+            notiType.Warning
+          )))
+        })
+        .finally(()=>{
+          setAllowUpdateToilet(true)
+          setUpdateToiletButtonText("EXPENSIVE! Update Toilets")
+        })
+    }
+  }
+
 
 
   return(
@@ -54,6 +100,9 @@ const SettingScreen = () => {
       </FieldContainer>
       <FieldContainer name={"Get in Touch"} >
         <TabLink itemName={"Found a bug?"} link={bugLink} />
+      </FieldContainer>
+      <FieldContainer name={"Demo"} defaultExpand={false}>
+        <Button text={updateToiletButtonText} colour={buttonColourRust} action={updateToilet} allowed={allowUpdateToilet}/>
       </FieldContainer>
     </div>
   )
